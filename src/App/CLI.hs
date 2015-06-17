f2d51dac911325d1169e8f5e5ec5c4bd941ab158
Tim Bodeit <tim@bodeit.com>
2015-06-17 19:56:56 +0200

    AC-8 Improved formatting. Fixed newlines for Windows users -.-

--- src/App/CLI.hs
+++ src/App/CLI.hs
@@ -114,10 +114,23 @@ finishIssueWithPullRequest issueKey = do
   brs <- liftGit Git.branchStatus
   wcs <- liftGit Git.workingCopyStatus
   case (brs, wcs) of
-    (NoUpstream, _    ) -> throwError $ UserInputException "Your current branch has not been pushed! Cannot create pull request on remote server."
-    (NewCommits, Clean) -> confirmFinish "You have new commits that haven't yet been pushed to the server.\nDo you with to continue\n[y/n] "
-    (NewCommits, Dirty) -> confirmFinish "You have new commits and changes that haven't been comitted yet.\nDo you with to continue\n[y/n] "
-    (        _ , Dirty) -> confirmFinish "You have changes that haven't been comitted yet.\nDo you with to continue\n[y/n] "
+    (NoUpstream, _    ) -> throwError $ UserInputException
+      "Your current branch has not been pushed! Cannot create pull request on remote server."
+    (NewCommits, Clean) -> confirmFinish  $
+      unlines [ "You have new commits that haven't yet been pushed to the server."
+              , "Do you with to continue"
+              , "[y/n] "
+              ]
+    (NewCommits, Dirty) -> confirmFinish $
+      unlines [ "You have new commits and changes that haven't been comitted yet."
+              , "Do you with to continue"
+              , "[y/n] "
+              ]
+    (        _ , Dirty) -> confirmFinish $
+      unlines [ "You have changes that haven't been comitted yet."
+              , "Do you with to continue"
+              , "[y/n] "
+              ]
     otherwise           -> finishIssueWithPullRequest' issueKey
   where
     confirmFinish message = do
