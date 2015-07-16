5956d84a7a98be2fef4a7b97e79152a2119aaab0
Daniel Strittmatter <daniel@smattr.de>
2015-07-17 00:01:46 +0200

    Fix issue key parser
    Before this commit, the parser would successfully parse a string of the form
    "-123" with an empty project name.

--- src/App/CLI/Parsers.hs
+++ src/App/CLI/Parsers.hs
@@ -3,8 +3,9 @@ module App.CLI.Parsers where
 import           App.Types
 import           App.Util
 
-import           Control.Applicative     ((<$>))
+import           Control.Applicative     hiding ((<|>))
 import           Control.Lens
+import           Control.Monad
 import           Data.Either.Combinators
 import           Jira.API                hiding (getConfig)
 import           Text.Parsec
@@ -12,19 +13,17 @@ import           Text.Parsec
 type StringParser a = Parsec String () a
 
 issueNumberParser :: StringParser IssueNumber
-issueNumberParser = IssueNumber . read <$> many1 digit
+issueNumberParser = IssueNumber . read <$> many1 digit <* eof
 
 issueKeyParser :: StringParser IssueKey
 issueKeyParser = do
-  project <- manyTill letter (char '-')
+  project <- many1Till letter (char '-')
   number  <- issueNumberParser
   return $ IssueKey project number
 
 issueKeyParserWithDefaultProject :: String -> StringParser IssueKey
 issueKeyParserWithDefaultProject project =
   issueKeyParser <|> IssueKey project <$> issueNumberParser
-
---
 
 parseIssueKey :: String -> AppM IssueKey
 parseIssueKey s = do
@@ -34,3 +33,11 @@ parseIssueKey s = do
   where
     parseException = UserInputException $
       "Not a valid issue identifier: " ++ s
+
+-- Helpers
+
+many1Till :: Stream s m t => ParsecT s u m a -> ParsecT s u m end -> ParsecT s u m [a]
+many1Till p end = do
+  r <- manyTill p end
+  guard . not $ null r
+  return r
